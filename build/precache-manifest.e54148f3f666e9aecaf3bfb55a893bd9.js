8a57091f45af7dc9e3dcd0920d3d48f76cdb2831
Robert Blaas <blaas.robert@gmail.com>
2019-02-07 20:00:25 +0000

    Added build files

---
+++ build/precache-manifest.e54148f3f666e9aecaf3bfb55a893bd9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9da57df5bdc0c091d5f7",
+    "url": "/Hopfensaft/react_shopping_cart/static/js/runtime~main.9da57df5.js"
+  },
+  {
+    "revision": "2f4a1ec9f51bfcd03573",
+    "url": "/Hopfensaft/react_shopping_cart/static/js/main.2f4a1ec9.chunk.js"
+  },
+  {
+    "revision": "20fdc1c12067c9b14745",
+    "url": "/Hopfensaft/react_shopping_cart/static/js/1.20fdc1c1.chunk.js"
+  },
+  {
+    "revision": "2f4a1ec9f51bfcd03573",
+    "url": "/Hopfensaft/react_shopping_cart/static/css/main.20978936.chunk.css"
+  },
+  {
+    "revision": "b527b213d7b7fe40aee63cf6b4828196",
+    "url": "/Hopfensaft/react_shopping_cart/index.html"
+  }
+];
